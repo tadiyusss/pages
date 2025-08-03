24483a2d45672b8f708f2029893b6ffae391a45d
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-08-03 18:08:49 +0800

    added role restriction on routes

--- routes/admin.py
+++ routes/admin.py
@@ -1,11 +1,18 @@
 from .. import bp
 from flask import render_template
+from core.utils.decorators import role_required
+from flask_login import login_required
+
 
 @bp.route('/dashboard/contents', methods=['GET'])
+@role_required('Administrator')
+@login_required
 def contents():
     return render_template('dashboard/contents.html')
 
 
 @bp.route('/dashboard/contents/create', methods=['GET'])
+@role_required('Administrator')
+@login_required
 def create_content():
     return render_template('dashboard/contents/create.html')
