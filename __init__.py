e2b1a73689a7032afee428c3d230e7c58e746028
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-08-03 13:50:37 +0800

    register sidebar and route imports

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 from flask import Blueprint
 from .metadata import TEMPLATE_FOLDER, STATIC_FOLDER
-from core.utils.registry.roles import register_role
-from core.utils.registry.side_navigation import register_sidebar_item, register_category
+from .initialization.sidebar import register_sidebar_items
 
 bp = Blueprint('pages', __name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER) 
 
-from . import routes
+from .routes import admin, public
 
 def init_extension(app, db):
     with app.app_context():
         db.create_all()
+        register_sidebar_items()
     return bp 
