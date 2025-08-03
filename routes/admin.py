4011a34951c4e9d6b728f41a97b1b5eac74a1223
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-08-03 17:49:25 +0800

    added route for create content

--- routes/admin.py
+++ routes/admin.py
@@ -4,3 +4,8 @@ from flask import render_template
 @bp.route('/dashboard/contents', methods=['GET'])
 def contents():
     return render_template('dashboard/contents.html')
+
+
+@bp.route('/dashboard/contents/create', methods=['GET'])
+def create_content():
+    return render_template('dashboard/contents/create.html')
